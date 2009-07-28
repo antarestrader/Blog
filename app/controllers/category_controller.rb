49728545ca0e57057e23bec0c6b17e19700efa8c
John Miller <jfmiller28@laptop.(none)>
2009-07-27 21:02:06 -0700

    Categories

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,10 +1,19 @@
 class CategoryController < Application
   
+  before :ensure_authenticated, :exclude => [:show]
+  
+  layout 'post_controller'
+  
   def show(id)
     category = Category.get(id)
     @headline = category.name
     @posts = category.posts.published
     render :template=>'post_controller/index', :layout=>'post_controller'
+  end
+  
+  def index
+    @categories = Category.all
+    render
   end
   
 end
