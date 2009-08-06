412cdf10bd4913b36b8d16860baf7f8f097f3614
John F. Miller <emperor@antarestrader.com>
2009-08-05 17:40:28 -0700

    Database secection working
    TODO: Rake tase to update individual databases

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -11,18 +11,26 @@ class Application < Merb::Controller
       @domain = Merb::Config[:domains][d]
     end
     
-    before :set_template_roots
-    after :restore_template_roots
+    before :set_domain
+    after :restore_domain
   end
   
-  def set_template_roots
+  def set_domain
     @_old_template_roots = self.class._template_roots
     raise NotFound, "expected to find a path at #{@domain[:template_root]}." unless @domain[:template_root]
     self.class._template_roots << [@domain[:template_root],:_template_location]
+    
+    if DataMapper::Repository.adapters.has_key?(@domain[:database])
+      DataMapper::Repository.context << DataMapper::repository(@domain[:database])
+    end
   end
   
-  def restore_template_roots
+  def restore_domain
     self.class._template_roots = @_old_template_roots
+    
+    if DataMapper::Repository.adapters.has_key?(@domain[:database])
+      DataMapper::Repository.context.pop
+    end
   end
   
 end
