1ed1b19e417f8a91a56e1dc80e31edb5b5546532
John F. Miller <emperor@antarestrader.com>
2009-08-14 11:17:52 -0700

    More logging for multidomains and bug fix

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,8 +1,12 @@
 class Application < Merb::Controller
 
   if Merb.config[:multidomain]
+    Merb.logger.info { "Setting up dispatch for Multiple Domains" }
+    Merb.logger.debug { "  domains: #{Merb::Config[:domains].inspect}" }
+
     before do
-      d = params[:domains] = request.domain(5)
+      d = params[:domain] = request.domain(5)
+      Merb.logger.debug { "  domain is: #{d}" }
       raise NotFound, "No Known Domain: #{d}" unless Merb::Config[:domains].has_key?(d)
       @domain = Merb::Config[:domains][d]
     end
@@ -17,19 +21,32 @@ class Application < Merb::Controller
   end
   
   def set_domain
-    @_old_template_roots = self.class._template_roots
+    @_old_template_roots = self.class._template_roots.dup
+    Merb.logger.debug { "  template root was:\n  #{@_old_template_roots.map{|i| i[0]}.join("\n  ")}" }
     raise NotFound, "expected to find a path at #{@domain[:template_root]}." unless @domain[:template_root]
+    Merb.logger.info { "Dispatching for domain: #{@domain[:name]} (#{@domain[:template_root]})" }
+   
     self.class._template_roots << [@domain[:template_root],:_template_location]
-    
-    if DataMapper::Repository.adapters.has_key?(@domain[:database])
-      DataMapper::Repository.context << DataMapper::repository(@domain[:database])
-    end
+    Merb.logger.debug { "  template root now:\n  #{self.class._template_roots.map{|i| i[0]}.join("\n  ")}" }
+    Merb.logger.debug { "  old template template root is:\n  #{@_old_template_roots.map{|i| i[0]}.join("\n  ")}" }
+    ::Application.set_repository(@domain[:database])
   end
   
   def restore_domain
     self.class._template_roots = @_old_template_roots
-    
-    if DataMapper::Repository.adapters.has_key?(@domain[:database])
+    Merb.logger.debug { "restoring template root: #{self.class._template_roots.inspect}" }
+    ::Application.reset_repository(@domain[:database])
+  end
+  
+  def self.set_repository(r)
+    if DataMapper::Repository.adapters.has_key?(r)
+      Merb.logger.debug { "  using database: #{r.inspect}" }
+      DataMapper::Repository.context << DataMapper::repository(r)
+    end
+  end
+  
+  def self.reset_repository(r=nil)
+    if DataMapper::Repository.adapters.has_key?(r) || r.nil?
       DataMapper::Repository.context.pop
     end
   end
