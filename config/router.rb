67e78c644c88c7b9674e89cf7d461d28079e27a8
John F. Miller <emperor@antarestrader.com>
2009-08-26 18:49:58 -0700

    Return a 406 for unknown query string on root
    This is to kill off crawlers the insist on checking old WordPress tages and categories and are now reaching the index, which is confusing them.  this should reduce unnecessary trafic to the site.

--- config/router.rb
+++ config/router.rb
@@ -15,6 +15,10 @@ Merb::Router.prepare do
   match('/feed/:action(.xml)').to(:controller=> 'feed_controller').name(:feeds)
   match('/',:query_string=>/feed=rss2/).to(:controller => 'feed_controller', :action =>'rss') #old wordpress format
   
+  match('/',:query_string=>/.+/).defer_to do |request, params|
+    raise Merb::ControllerExceptions::NotAcceptable, "Query String Unknown: #{request.query_string}"
+  end
+  
   # Adds the required routes for merb-auth using the password slice
   slice(:merb_auth_slice_password, :name_prefix => nil, :path_prefix => "")
   
